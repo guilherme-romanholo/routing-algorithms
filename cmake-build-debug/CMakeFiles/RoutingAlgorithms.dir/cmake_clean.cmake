3936a73fe5bb0b7f5e183bc257774a68d0255150
William Martins <williamdabruzzmartins76136@gmail.com>
2023-06-09 17:11:03 -0300

    teste
    aa

---
+++ cmake-build-debug/CMakeFiles/RoutingAlgorithms.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RoutingAlgorithms.dir/src/components/ip.c.o"
+  "CMakeFiles/RoutingAlgorithms.dir/src/components/ip.c.o.d"
+  "CMakeFiles/RoutingAlgorithms.dir/src/main/main.c.o"
+  "CMakeFiles/RoutingAlgorithms.dir/src/main/main.c.o.d"
+  "RoutingAlgorithms"
+  "RoutingAlgorithms.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/RoutingAlgorithms.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
