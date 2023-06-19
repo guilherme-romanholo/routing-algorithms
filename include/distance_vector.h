60bd008ec6f12c67394ed2377efd9af2a31f9823
guilherme-romanholo <guilhermeromanholobofo@gmail.com>
2023-06-18 22:58:38 -0300

    distance vector está pronto

--- include/distance_vector.h
+++ include/distance_vector.h
@@ -5,4 +5,60 @@
 #ifndef ROUTINGALGORITHMS_DISTANCE_VECTOR_H
 #define ROUTINGALGORITHMS_DISTANCE_VECTOR_H
 
+/**
+ * Estrurura referente as arestas do grafo
+ */
+struct Edge {
+    int source;
+    int destination;
+    int weight;
+};
+
+/**
+ * Função para incializar as arestas do grafo
+ * @param distances Vetor de distâncias
+ * @param predecessors Vetor dos predecessores
+ * @param numNodes Número de vértices
+ * @param sourceNode Vértice de origem
+ */
+void initializeDistances(int distances[], int predecessors[], int numNodes, int sourceNode);
+
+/**
+ * Função para imprimir a tabela de roteamento
+ * @param distances Vetor de distâncias
+ * @param predecessors Vetor de predecessores
+ * @param numNodes Número de vértices
+ */
+void printRoutingTable(int distances[], int predecessors[], int numNodes);
+
+/**
+ * Função para atualizar o peso das arestas conforme o pacote percorre o grafo
+ * @param edges Vetor das arestas
+ * @param source Posição de origem do pacote
+ * @param destination Posiçaõ de destino do pacote
+ * @param numEdges Número de arestas
+ * @param packageWeight Peso do pacote
+ */
+void updateWeight(struct Edge edges[], int source, int destination, int numEdges, int packageWeight);
+
+/**
+ * Função para printar o caminho que o pacote faz da origem até o destino
+ * @param edges Vetor de arestas
+ * @param predecessors Vetor de predecessores
+ * @param destinationNode Nó de destino
+ * @param numEdges Número de arestas
+ * @param packageWeight Peso do pacote
+ */
+void printPath(struct Edge edges[], int predecessors[], int destinationNode, int numEdges, int packageWeight);
+
+/**
+ * Função principal para calcular o caminho mínimo pelo algoritimo distance vector
+ * @param edges Vetor de arestas
+ * @param numEdges Número de arestas
+ * @param numNodes Número de nós
+ * @param sourceNode Nó de origem
+ * @param destinationNode Nó de destino
+ */
+void distanceVector(struct Edge edges[], int numEdges, int numNodes, int sourceNode, int destinationNode);
+
 #endif //ROUTINGALGORITHMS_DISTANCE_VECTOR_H
--- src/routing/distance_vector.c
+++ src/routing/distance_vector.c
@@ -1,108 +1,134 @@
 // Código fonte para implementação do algoritmo distance vector
 
+#include <limits.h>
+#include <stdio.h>
 #include "../../include/distance_vector.h"
 
-/*
-#include<stdio.h>
-struct node
-{
-    unsigned dist[20];
-    unsigned from[20];
-}rt[10];
-int main()
-{
-    int costmat[20][20];
-    int nodes,i,j,k,count=0;
-    printf("\nEnter the number of nodes : ");
-    scanf("%d",&nodes);//Enter the nodes
-    printf("\nEnter the cost matrix :\n");
-    for(i=0;i<nodes;i++)
-    {
-        for(j=0;j<nodes;j++)
-        {
-            scanf("%d",&costmat[i][j]);
-            costmat[i][i]=0;
-            rt[i].dist[j]=costmat[i][j];//initialise the distance equal to cost matrix
-            rt[i].from[j]=j;
+#define MAX_NODES 100
+
+void initializeDistances(int distances[], int predecessors[], int numNodes, int sourceNode) {
+    // Laço para iniciar as arestas
+    for (int i = 0; i < numNodes; i++) {
+        // Caso for o vértice de origem inicializa com zero, se não com infinito
+        if (i == sourceNode) {
+            distances[i] = 0;
+        } else {
+            distances[i] = INT_MAX;
         }
+        // Incializa sem predecessores
+        predecessors[i] = -1;
     }
-
-    do
-    {
-        count=0;
-        for(i=0;i<nodes;i++)//We choose arbitary vertex k and we calculate the direct distance from the node i to k using the cost matrix
-            //and add the distance from k to node j
-            for(j=0;j<nodes;j++)
-                for(k=0;k<nodes;k++)
-                    if(rt[i].dist[j]>costmat[i][k]+rt[k].dist[j])
-                    {//We calculate the minimum distance
-                        rt[i].dist[j]=rt[i].dist[k]+rt[k].dist[j];
-                        rt[i].from[j]=k;
-                        count++;
-                    }
-    }while(count!=0);
-
-    for(i=0;i<nodes;i++)
-    {
-        printf("\n\n For router %d\n",i+1);
-        for(j=0;j<nodes;j++)
-        {
-            printf("\t\nnode %d via %d Distance %d ",j+1,rt[i].from[j]+1,rt[i].dist[j]);
-        }
-    }
-
-    printf("\n\n");
-    getch();
 }
-#include<stdio.h>
-struct node
-{
-    unsigned dist[20];
-    unsigned from[20];
-}rt[10];
-int main()
-{
-    int costmat[20][20];
-    int nodes,i,j,k,count=0;
-    printf("\nEnter the number of nodes : ");
-    scanf("%d",&nodes);//Enter the nodes
-    printf("\nEnter the cost matrix :\n");
-    for(i=0;i<nodes;i++)
-    {
-        for(j=0;j<nodes;j++)
-        {
-            scanf("%d",&costmat[i][j]);
-            costmat[i][i]=0;
-            rt[i].dist[j]=costmat[i][j];//initialise the distance equal to cost matrix
-            rt[i].from[j]=j;
+
+void printRoutingTable(int distances[], int predecessors[], int numNodes) {
+    printf("Tabela de Roteamento:\n");
+    printf("Destino\t\tPróximo Salto\tDistância\n");
+    for (int i = 0; i < numNodes; i++) {
+        printf("%d\t\t", i);
+        // Verifica se tem predecessor para printar
+        if (predecessors[i] != -1) {
+            printf("%d\t\t", predecessors[i]);
+        } else {
+            printf("-\t\t");
         }
-    }
-
-    do
-    {
-        count=0;
-        for(i=0;i<nodes;i++)//We choose arbitary vertex k and we calculate the direct distance from the node i to k using the cost matrix
-            //and add the distance from k to node j
-            for(j=0;j<nodes;j++)
-                for(k=0;k<nodes;k++)
-                    if(rt[i].dist[j]>costmat[i][k]+rt[k].dist[j])
-                    {//We calculate the minimum distance
-                        rt[i].dist[j]=rt[i].dist[k]+rt[k].dist[j];
-                        rt[i].from[j]=k;
-                        count++;
-                    }
-    }while(count!=0);
-
-    for(i=0;i<nodes;i++)
-    {
-        printf("\n\n For router %d\n",i+1);
-        for(j=0;j<nodes;j++)
-        {
-            printf("\t\nnode %d via %d Distance %d ",j+1,rt[i].from[j]+1,rt[i].dist[j]);
+        // Verificar se possui alguma distancia não infinita
+        if (distances[i] != INT_MAX) {
+            printf("%d", distances[i]);
+        } else {
+            printf("-");
         }
+        printf("\n");
     }
-
-    printf("\n\n");
-    getch();
+    printf("\n");
 }
- */
+
+void updateWeight(struct Edge edges[], int source, int destination, int numEdges, int packageWeight) {
+    // Percorre os nós e vai incrementando o valor das arestas
+    for (int i = 0; i < numEdges; i++) {
+        if (edges[i].source == source && edges[i].destination == destination) {
+            edges[i].weight += packageWeight;
+        }
+    }
+}
+
+void printPath(struct Edge edges[], int predecessors[], int destinationNode, int numEdges, int packageWeight) {
+    // Caso de parada da recursão quando chega no destino
+    if (predecessors[destinationNode] == -1) {
+        printf("%d ", destinationNode);
+        return;
+    }
+
+    // Chamada recursiva com o nó de origem sendo o destino
+    printPath(edges, predecessors, predecessors[destinationNode], numEdges, packageWeight);
+
+    // Recalcula o peso da aresta percorrida
+    updateWeight(edges, predecessors[destinationNode], destinationNode, numEdges, packageWeight);
+
+    // Printa o caminho
+    printf("%d ", destinationNode);
+}
+
+void distanceVector(struct Edge edges[], int numEdges, int numNodes, int sourceNode, int destinationNode) {
+    // Define os vetor de distância e de predecessores
+    int distances[MAX_NODES];
+    int predecessors[MAX_NODES];
+
+    // Inicializa as distâncias
+    initializeDistances(distances, predecessors, numNodes, sourceNode);
+
+    // Executa o algoritmo de Bellman-Ford
+    for (int i = 0; i < numNodes - 1; i++) {
+        for (int j = 0; j < numEdges; j++) {
+            int u = edges[j].source;
+            int v = edges[j].destination;
+            int weight = edges[j].weight;
+
+            if (distances[u] != INT_MAX && distances[u] + weight < distances[v]) {
+                distances[v] = distances[u] + weight;
+                predecessors[v] = u;
+            }
+        }
+        // Imprime a tabela de roteamento
+        printf("Hop %d:\n", i + 1);
+        printRoutingTable(distances, predecessors, numNodes);
+    }
+
+    // Verifica se há ciclos de peso negativo
+    for (int i = 0; i < numEdges; i++) {
+        int u = edges[i].source;
+        int v = edges[i].destination;
+        int weight = edges[i].weight;
+
+        if (distances[u] != INT_MAX && distances[u] + weight < distances[v]) {
+            printf("Ciclo de peso negativo detectado!\n");
+            return;
+        }
+    }
+
+    // Imprime o caminho mínimo e os "hops" até o nó de destino
+    printf("Caminho mínimo do nó %d ao nó %d:\n", sourceNode, destinationNode);
+    printf("Caminho: ");
+    printPath(edges, predecessors, destinationNode, numEdges, 5);
+    printf("\nDistância: %d\n", distances[destinationNode]);
+}
+
+// Passar os pacotes aqui
+void prepareDistanceVector(int sourceNode, int destinationNode /*, Grafo de roteadores, pode ser até uma matriz*/) {
+    // Exemplo de grafo que será montado a partir do arquivo
+    struct Edge edges[] = {{0, 1, 1},
+                           {0, 2, 2},
+                           {1, 3, 1},
+                           {1, 4, 5},
+                           {2, 4, 7},
+                           {3, 4, 8},
+                           {3, 5, 1},
+                           {5, 4, 1}};
+    // Número de arestas continua calculando assim
+    int numEdges = sizeof(edges) / sizeof(edges[0]);
+    // Número de nós obtemos a partir do arquivo
+    int numNodes = 6;
+
+    distanceVector(edges, numEdges, numNodes, sourceNode, destinationNode);
+    //Chamo a função para recalcular o caminho mínimo após aumentar os pesos com o peso do pacote
+    // distanceVector(edges, numEdges, numNodes, sourceNode, destinationNode);
+}
